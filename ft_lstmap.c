819e554e0ad54a1be20b81f55424c615a3e14be2
Aline Morais <moraisraline@gmail.com>
2022-03-09 14:42:45 +0000

    ft_lstmap

---
+++ ft_lstmap.c
@@ -0,0 +1,105 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lstmap.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ados-rei <ados-rei@student.42lisboa.com>   +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/03/05 18:01:32 by ados-rei          #+#    #+#             */
+/*   Updated: 2022/03/08 13:36:34 by ados-rei         ###   ########.fr       */
+/*                                                                            */
+/*   -> Iterates the list lst and applies the function f on the content of    */
+/*      each node.                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
+{
+	t_list	*new_list;
+	t_list	*elem;
+
+	new_list = NULL;
+	while (lst)
+	{
+		elem = ft_lstnew((*f)(lst->content));
+		if (elem == NULL)
+		{
+			ft_lstclear(&new_list, del);
+			return (NULL);
+		}
+		ft_lstadd_back(&new_list, elem);
+		lst = lst->next;
+	}
+	return (new_list);
+}
+
+/*    TESTING   */
+/*#include <string.h>
+void	ft_print_result(t_list *elem)
+{
+	int		len;
+
+	len = 0;
+	while (((char *)elem->content)[len])
+		len++;
+	write(1, ((char *)elem->content), len);
+	write(1, "\n", 1);
+}
+
+void	*ft_map(void *ct)
+{
+	int i;
+	void	*c;
+	char	*pouet;
+
+	c = ct;
+	i = -1;
+	pouet = (char *)c;
+	while (pouet[++i])
+		if (pouet[i] == 'o')
+			pouet[i] = 'a';
+	return (c);
+}
+
+void    ft_del(void *content)
+{
+	free(content);
+}
+
+int main()
+{
+	t_list		*elem;
+	t_list		*elem2;
+	t_list		*elem3;
+	t_list		*elem4;
+	t_list		*list;
+	char		*str = strdup("lorem");
+	char		*str2 = strdup("ipsum");
+	char		*str3 = strdup("dolor");
+	char		*str4 = strdup("sit");
+
+	elem = ft_lstnew(str);
+	elem2 = ft_lstnew(str2);
+	elem3 = ft_lstnew(str3);
+	elem4 = ft_lstnew(str4);
+
+	elem->next = elem2;
+	elem2->next = elem3;
+	elem3->next = elem4;
+
+	if (!(list = ft_lstmap(elem, &ft_map, &ft_del)))
+		return (0);
+	if (list == elem)
+		write(1, "A new list is not returned\n", 27);
+	int i;
+	i = 0;
+	ft_print_result(list);
+	while (list->next)
+	{
+		list = list->next;
+		ft_print_result(list);
+		i++;
+	}
+    return (0);
+}*/
