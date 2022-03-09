aa9e730d4971fb45bfc1543b1443d1b196664747
Aline Morais <moraisraline@gmail.com>
2022-03-09 11:55:31 +0000

    ft_putnbr_fd

---
+++ ft_putnbr_fd.c
@@ -0,0 +1,45 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_putnbr_fd.c                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ados-rei <ados-rei@student.42lisboa.com>   +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/02/24 16:14:09 by ados-rei          #+#    #+#             */
+/*   Updated: 2022/02/27 18:25:39 by ados-rei         ###   ########.fr       */
+/*                                                                            */
+/*   -> Outputs the integer n to the given file descriptor.                   */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+static void	ft_putchar(char c, int fd)
+{
+	write(fd, &c, 1);
+}
+
+void	ft_putnbr_fd(int n, int fd)
+{
+	if (n == -2147483648)
+	{
+		ft_putnbr_fd(n / 10, fd);
+		ft_putchar('8', fd);
+	}
+	else if (n < 0)
+	{
+		ft_putchar('-', fd);
+		ft_putnbr_fd(-n, fd);
+	}
+	else
+	{
+		if (n > 9)
+			ft_putnbr_fd(n / 10, fd);
+		ft_putchar(n % 10 + 48, fd);
+	}
+}
+
+/*    TESTING   */
+/*int main ()
+{
+	ft_putnbr_fd(-10, 1);
+}*/
